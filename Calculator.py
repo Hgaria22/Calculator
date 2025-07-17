1c29bf841d86530c2521a6beea1d46822cf2a1d3
Hgaria22 <gariahrishit@gmail.com>
2025-07-17 13:18:12 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,10 @@
 
 
 operator = input("Enter an operator (+ - * /): ")
+# To choose an operator for your calculation
 num1 = float(input("Choose your first number: "))
 num2 = float(input("Choose your second number: "))
-
+#Choose two numbers to use the operator and find out the result
 
 if operator == '+':
     print(float(num1 + num2))
@@ -18,4 +19,5 @@ elif operator == '/':
         print(float(num1 / num2))
 else:
     print(f"{operator} isn't a valid operator")
+#To tell user any random character isn't a valid operator
 
